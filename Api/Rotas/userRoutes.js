e3f1af6f92b34277e2b8b338510364e01d3f8949
Laura <laura.antunes@gmail.com>
2024-11-07 11:18:38 -0300

    add rotas put e delete

--- Api/Rotas/userRoutes.js
+++ Api/Rotas/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// Armazenando em memória (simulando banco de dados)
 let users = [];
 
 // Rota para cadastro de usuário
@@ -17,8 +18,6 @@ router.post('/cadastro', (req, res) => {
     res.status(201).json({ message: 'Usuário cadastrado com sucesso!', user: newUser });
 });
 
-module.exports = router;
-
 // Rota para consulta de usuários (retornar todos os usuários cadastrados)
 router.get('/consulta', (req, res) => {
   if (users.length === 0) {
@@ -27,5 +26,39 @@ router.get('/consulta', (req, res) => {
 
   res.status(200).json(users);
 });
+
+// Rota para atualização de usuário pelo ID
+router.put('/atualizacao/:id', (req, res)=>{
+    const { id } = req.params;
+    const { nome, email, senha } = req.body;
+
+    const userIndex = users.findIndex((user) => user.id === parseInt(id));
+
+    if (userIndex === -1) {
+        return res.status(400).json({ message: "Usuário não encontrado." });
+    }
+
+    if (!nome || !email || !senha) {
+        return res.status(400).json({ message: 'Nome, email e senha são obrigatórios.' });
+    }
+    
+    users[userIndex] = { id: parseInt(id), nome, email, senha };
+    res.status(200).json({ message: 'Usuário atualizado com sucesso!', user: users[userIndex] }); 
+});
+
+// Rota para deletar um usuário pelo ID
+router.delete('/deletar/:id', (req, res) => {
+    const { id } = req.params;
+    const userIndex = users.findIndex((user) => user.id === parseInt(id));
+
+    if (userIndex === -1) {
+    return res.status(404).json({ message: 'Usuário não encontrado.' });
+    }
+
+    // Remover o usuário do array
+    users.splice(userIndex, 1);
+    res.status(200).json({ message: 'Usuário deletado com sucesso.' });
+});
+router.delete
 
 module.exports = router;
